b1d14c1ac644f3ec9657280fe2ec0ed40c54a214
devikas48 <51810081+devikas48@users.noreply.github.com>
2020-05-01 16:56:07 -0700

    nasty surprise

---
+++ src/_01_nasty_surprise/NastySurprise.java
@@ -0,0 +1,19 @@
+package _01_nasty_surprise;
+
+import javax.swing.JFrame;
+
+public class NastySurprise {
+public static void main(String[] args) {
+	
+	JFrame frame = new JFrame();
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
