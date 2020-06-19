387536759a70aadfe809cfdd97d5fc90fa6f8745
devikas48 <51810081+devikas48@users.noreply.github.com>
2020-06-19 16:55:48 -0700

    typing tutor

--- src/_05_typing_tutor/TypingTutor.java
+++ src/_05_typing_tutor/TypingTutor.java
@@ -1,31 +1,61 @@
 package _05_typing_tutor;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Random;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class TypingTutor {
+public class TypingTutor implements KeyListener {
 	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	private char currentLetter;
+	char generateRandomLetter() {
+	    Random r = new Random();
+	    return (char) (r.nextInt(26) + 'a');
+	}
 	
+
 	
 public void run() {
 	JFrame frame  = new JFrame();
 	frame.setVisible(true);
 	frame.setTitle("Type or Die");
+	currentLetter = generateRandomLetter();
+	label.setText("" + currentLetter);
+	label.setFont(label.getFont().deriveFont(28.0f));
+	label.setHorizontalAlignment(JLabel.CENTER);
+	frame.addKeyListener(this);
 }
 	
 	
-	
-	
-	
-	
+
 public static void main(String[] args) {
 	new TypingTutor().run();
+}
+
+
+@Override
+public void keyReleased(KeyEvent e) {
+	// TODO Auto-generated method stub
 	
+}
+
+
+
+@Override
+public void keyTyped(KeyEvent e) {
+	// TODO Auto-generated method stub
 	
-	
-	
-	
-	
+}
+
+
+
+@Override
+public void keyPressed(KeyEvent e) {
+	// TODO Auto-generated method stub
 	
 }
 }
