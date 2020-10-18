e2e4e81d6c30d6ef20a80ebf93c40e1e8b72014b
devikas48 <51810081+devikas48@users.noreply.github.com>
2020-10-17 17:30:41 -0700

    practice quiz

--- src/_12_slot_machine/SlotMachine.java
+++ src/_12_slot_machine/SlotMachine.java
@@ -1,15 +1,18 @@
 package _12_slot_machine;
 
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Random;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class SlotMachine implements ActionListener {
@@ -21,7 +24,7 @@ JLabel label3 = new JLabel();
 JButton button = new JButton();
 
 public void run() {
-	frame.add(panel);
+	panel.add(button);
 	button.setText("SPIN");
 	frame.setVisible(true);
 	button.addActionListener(this);
@@ -33,21 +36,25 @@ public void run() {
 		e.printStackTrace();
 	}
 	try {
-	label2 = createLabelImage("lime.jpeg");
+	label2 = createLabelImage("lime.png");
 	} catch (MalformedURLException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
 	try {
-	label3 = createLabelImage("orange.png");
+	label3 = createLabelImage("orange.jpg");
 	} catch (MalformedURLException e) {
 	// TODO Auto-generated catch block
 		e.printStackTrace();
 
-	}  
+	} 
+	label.setPreferredSize(new Dimension (300, 300));
+	label2.setPreferredSize(new Dimension (300, 300));
+	label3.setPreferredSize(new Dimension (300, 300));
 	panel.add(label);
 	panel.add(label2);
 	panel.add(label3);
+	frame.add(panel);
 	frame.pack();
 }
 
@@ -56,6 +63,109 @@ public void run() {
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
 	JButton buttonPressed = (JButton) e.getSource();
+	Random ran  = new Random();
+	int number1 = ran.nextInt(3);
+	int number2 = ran.nextInt(3);
+	int number3 = ran.nextInt(3);
+	
+	panel.removeAll();
+	
+	if(number1 == 0) {
+		try {
+			label = createLabelImage("cherry.jpg");
+			} catch (MalformedURLException d) {
+				// TODO Auto-generated catch block
+				d.printStackTrace();
+			}
+	}
+	
+	if(number1 == 1) {
+		try {
+			label = createLabelImage("lime.png");
+			} catch (MalformedURLException d) {
+				// TODO Auto-generated catch block
+				d.printStackTrace();
+			}
+	}
+	
+	if(number1 == 2) {
+		try {
+			label = createLabelImage("orange.jpg");
+			} catch (MalformedURLException d) {
+			// TODO Auto-generated catch block
+				d.printStackTrace();
+			}
+	}
+	
+	if(number2 == 0) {
+		try {
+			label2 = createLabelImage("cherry.jpg");
+			} catch (MalformedURLException d) {
+				// TODO Auto-generated catch block
+				d.printStackTrace();
+			}
+	}
+	
+	if(number2 == 1) {
+		try {
+			label2 = createLabelImage("lime.png");
+			} catch (MalformedURLException d) {
+				// TODO Auto-generated catch block
+				d.printStackTrace();
+			}
+	}
+	
+	if(number2 == 2) {
+		try {
+			label2 = createLabelImage("orange.jpg");
+			} catch (MalformedURLException d) {
+			// TODO Auto-generated catch block
+				d.printStackTrace();
+			}
+	}
+	
+	if(number3 == 0) {
+		try {
+			label3 = createLabelImage("cherry.jpg");
+			} catch (MalformedURLException d) {
+				// TODO Auto-generated catch block
+				d.printStackTrace();
+			}
+	}
+	
+	if(number3 == 1) {
+		try {
+			label3 = createLabelImage("lime.png");
+			} catch (MalformedURLException d) {
+				// TODO Auto-generated catch block
+				d.printStackTrace();
+			}
+	}
+	
+	if(number3 == 2) {
+		try {
+			label3 = createLabelImage("orange.jpg");
+			} catch (MalformedURLException d) {
+			// TODO Auto-generated catch block
+				d.printStackTrace();
+			}
+	}
+	
+	
+	
+	label.setPreferredSize(new Dimension (300, 300));
+	label2.setPreferredSize(new Dimension (300, 300));
+	label3.setPreferredSize(new Dimension (300, 300));
+	panel.add(label);
+	panel.add(label2);
+	panel.add(label3); 
+	panel.add(button);
+	frame.pack();
+	
+	if( number1 == number2 && number2 == number3) {
+		JOptionPane.showMessageDialog(null, "YOU WIN!");
+	}
+	
 	
 }
 
